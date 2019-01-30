fd173ca91ebcbcda30217aac70721c9836a6c6bf
Nathaniel Capule <ncapule@google.com>
2019-01-30 22:40:48 +0800

    go js wasm exec

---
+++ snippets/gopherjs.go
@@ -0,0 +1,19 @@
+package main
+ // For interacting with DOM.
+import "github.com/gopherjs/gopherjs/js"
+
+func main() {
+  js.Global.Set("pet", map[string]interface{}{"New": New})
+}
+
+type Pet struct {
+  name string
+}
+
+func New(name string) *js.Object {
+  return js.MakeWrapper(&Pet{name})
+}
+
+func (p *Pet) Name() string {
+  return p.name
+}
